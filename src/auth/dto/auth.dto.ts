c356c623387a05232e8cf2e1a7b108db437bf4e9
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-10-27 14:45:35 -0300

    customizando mensagens de erro #12

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -16,60 +16,75 @@ export const TiposDeUsuario: TipoDeUsuario[] = ['ADMINISTRADOR', 'CADASTRADOR'];
 export const TiposDeBeneficiario: TipoDeBeneficiario[] = ['BENEFICIARIO'];
 
 export class SignUpUsuarioDto {
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: 'nome não deve ser omitido' })
+  @IsString({ message: 'nome deve ser uma string' })
   nome: string;
 
-  @IsEmail()
-  @IsString()
+  @IsEmail({}, { message: 'email deve ser um email válido' })
+  @IsString({ message: 'email deve ser uma string' })
   email: string;
 
   @IsOptional()
-  @IsString()
-  @IsPhoneNumber('BR')
+  @IsString({ message: 'telefone deve ser uma string' })
+  @IsPhoneNumber('BR', {
+    message: 'telefone deve ter o formato <CODIGO_DO_PAIS><DDD>xxxxxxxx',
+  })
   telefone: string;
 
-  @IsEnum(TiposDeUsuario)
+  @IsEnum(TiposDeUsuario, {
+    message: 'tipo deve ser ADMINISTRADOR ou CADASTRADOR',
+  })
   tipo: TipoDeUsuario;
 
-  @IsString()
-  @IsStrongPassword()
+  @IsStrongPassword(
+    {},
+    {
+      message:
+        'senha deve conter no mínimo 8 caracteres, 1 letra mínuscula, 1 maíusucula, 1 número e 1 caractere especial',
+    },
+  )
   senha: string;
 }
 
 export class SignUpBeneficiarioDto {
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: 'nome não pode ser omitido' })
+  @IsString({ message: 'nome deve ser uma string' })
   nome: string;
 
-  @IsString()
-  @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/)
+  @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/, {
+    message:
+      'cpf deve  ter os nove primeiros dígitos agrupados em três grupos de três dígitos separados por um ponto, seguidos de um hífen e dos dois últimos dígitos',
+  })
   cpf: string;
 
-  @IsEmail()
-  @IsString()
+  @IsEmail({}, { message: 'email deve ser um email válido' })
   email: string;
 
   @IsOptional()
-  @IsString()
-  @IsPhoneNumber('BR')
+  @IsPhoneNumber('BR', {
+    message: 'telefone deve ter o formato <CODIGO_DO_PAIS><DDD>XXXXXXXX',
+  })
   telefone: string;
 
-  @IsEnum(TiposDeBeneficiario)
+  @IsEnum(TiposDeBeneficiario, { message: 'tipo deve ser BENEFICIARIO' })
   tipo: TipoDeBeneficiario;
 
-  @IsString()
-  @IsStrongPassword()
+  @IsStrongPassword(
+    {},
+    {
+      message:
+        'senha deve conter no mínimo 8 caracteres, 1 letra mínuscula, 1 maíusucula, 1 número e 1 caractere especial',
+    },
+  )
   senha: string;
 }
 
 export class SignInUsuarioDto {
-  @IsEmail()
-  @IsString()
+  @IsEmail({}, { message: 'email deve ser um email válido' })
   email: string;
 
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: 'senha não pode ser omitida' })
+  @IsString({ message: 'senha deve ser uma string' })
   senha: string;
 }
 
@@ -77,17 +92,16 @@ export class SignInBeneficiarioDto {
   @IsString()
   @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/, {
     message:
-      'CPF deve  ter os nove primeiros dígitos agrupados em três grupos de três dígitos separados por um ponto, seguidos de um hífen e dos dois últimos dígitos',
+      'cpf deve  ter os nove primeiros dígitos agrupados em três grupos de três dígitos separados por um ponto, seguidos de um hífen e dos dois últimos dígitos',
   })
   @IsOptional()
   cpf?: string;
 
-  @IsEmail()
-  @IsString()
+  @IsEmail({}, { message: 'email deve ser um email válido' })
   @IsOptional()
   email?: string;
 
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: 'senha não pode ser omitida' })
+  @IsString({ message: 'senha deve ser uma string' })
   senha: string;
 }
