fb1666ef6ab8e94931fd83ac773c1aa11b618dd9
Iev Mesquita <ievsouza622@gmail.com>
2023-11-06 20:15:36 -0300

    Adicionando Decorators para estruturação da documentação com swagger #22

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,27 +6,34 @@ import {
   SignUpBeneficiarioDto,
   SignUpUsuarioDto,
 } from './dto/auth.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Cadastro e Login')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Cadastra um novo Usuário' })
   @Post('signup/usuario')
   signUpUsuario(@Body() dto: SignUpUsuarioDto) {
     return this.authService.signUpUsuario(dto);
   }
 
+  @ApiOperation({ summary: 'Loga um Usuário' })
   @HttpCode(HttpStatus.OK)
   @Post('signin/usuario')
   signInUsuario(@Body() dto: SignInUsuarioDto) {
     return this.authService.signInUsuario(dto);
   }
 
+  @ApiOperation({ summary: 'Cadastra um novo Beneficiário' })
   @Post('signup/beneficiario')
   signUpBeneficiario(@Body() dto: SignUpBeneficiarioDto) {
     return this.authService.signUpBeneficiario(dto);
   }
 
+  @ApiOperation({ summary: 'Loga um Beneficiário' })
   @HttpCode(HttpStatus.OK)
   @Post('signin/beneficiario')
   signInBeneficiario(@Body() dto: SignInBeneficiarioDto) {
