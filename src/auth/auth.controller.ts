462b36d9a4a7dd9a5cf7f62d3b0ba1ece3f91e16
Iev Mesquita <ievsouza622@gmail.com>
2023-11-06 21:24:27 -0300

    Atualizando descrição e sumário da documentação de usuários #22

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,34 +6,48 @@ import {
   SignUpBeneficiarioDto,
   SignUpUsuarioDto,
 } from './dto/auth.dto';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@ApiBearerAuth()
 @ApiTags('Cadastro e Login')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @ApiOperation({ summary: 'Cadastra um novo Usuário' })
+  @ApiOperation({
+    summary: 'Cadastra um novo Usuário',
+    description: 'Cria um novo usuário e o grava em banco de dados',
+  })
   @Post('signup/usuario')
   signUpUsuario(@Body() dto: SignUpUsuarioDto) {
     return this.authService.signUpUsuario(dto);
   }
 
-  @ApiOperation({ summary: 'Loga um Usuário' })
+  @ApiOperation({
+    summary: 'Login de Usuário',
+    description:
+      'Autentica um usuário, e, caso encontrado em banco de dados, permite o acesso desse usuário',
+  })
   @HttpCode(HttpStatus.OK)
   @Post('signin/usuario')
   signInUsuario(@Body() dto: SignInUsuarioDto) {
     return this.authService.signInUsuario(dto);
   }
 
-  @ApiOperation({ summary: 'Cadastra um novo Beneficiário' })
+  @ApiOperation({
+    summary: 'Cadastra um novo Beneficiário',
+    description:
+      'Cria um novo usuário "Beneficiário" e o grava em banco de dados',
+  })
   @Post('signup/beneficiario')
   signUpBeneficiario(@Body() dto: SignUpBeneficiarioDto) {
     return this.authService.signUpBeneficiario(dto);
   }
 
-  @ApiOperation({ summary: 'Loga um Beneficiário' })
+  @ApiOperation({
+    summary: 'Loga um Beneficiário',
+    description:
+      'Autentica um usuário "Beneficiário", e, caso encontrado em banco de dados, permite o acesso desse usuário',
+  })
   @HttpCode(HttpStatus.OK)
   @Post('signin/beneficiario')
   signInBeneficiario(@Body() dto: SignInBeneficiarioDto) {
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -8,7 +8,7 @@ import {
   IsStrongPassword,
   Matches,
 } from 'class-validator';
-import {  ApiProperty } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
 export type TipoDeUsuario = 'ADMINISTRADOR' | 'CADASTRADOR';
 export type TipoDeBeneficiario = 'BENEFICIARIO';
@@ -17,17 +17,17 @@ export const TiposDeUsuario: TipoDeUsuario[] = ['ADMINISTRADOR', 'CADASTRADOR'];
 export const TiposDeBeneficiario: TipoDeBeneficiario[] = ['BENEFICIARIO'];
 
 export class SignUpUsuarioDto {
-  @ApiProperty({example: "Cleber Guimarães"})
+  @ApiProperty({ example: 'Cleber Guimarães' })
   @IsNotEmpty({ message: 'nome não deve ser omitido' })
   @IsString({ message: 'nome deve ser uma string' })
   nome: string;
 
-  @ApiProperty({example: "cleber.guimaraes@email.com"})
+  @ApiProperty({ example: 'cleber.guimaraes@email.com' })
   @IsEmail({}, { message: 'email deve ser um email válido' })
   @IsString({ message: 'email deve ser uma string' })
   email: string;
 
-  @ApiProperty({example: "+5561995435997"})
+  @ApiProperty({ example: '+5561995435997' })
   @IsOptional()
   @IsString({ message: 'telefone deve ser uma string' })
   @IsPhoneNumber('BR', {
@@ -35,13 +35,16 @@ export class SignUpUsuarioDto {
   })
   telefone: string;
 
-  @ApiProperty({ enum: ['ADMINISTRADOR', 'CADASTRADOR'], example: "ADMINISTRADOR" })
+  @ApiProperty({
+    enum: ['ADMINISTRADOR', 'CADASTRADOR'],
+    example: 'ADMINISTRADOR',
+  })
   @IsEnum(TiposDeUsuario, {
     message: 'tipo deve ser ADMINISTRADOR ou CADASTRADOR',
   })
   tipo: TipoDeUsuario;
 
-  @ApiProperty({example: "Senha123!"})
+  @ApiProperty({ example: 'Senha123!' })
   @IsStrongPassword(
     {},
     {
@@ -53,34 +56,34 @@ export class SignUpUsuarioDto {
 }
 
 export class SignUpBeneficiarioDto {
-  @ApiProperty({example: "Robson Paiva"})
+  @ApiProperty({ example: 'Robson Paiva' })
   @IsNotEmpty({ message: 'nome não pode ser omitido' })
   @IsString({ message: 'nome deve ser uma string' })
   nome: string;
 
-  @ApiProperty({example: "123.456.789-10"})
+  @ApiProperty({ example: '123.456.789-10' })
   @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/, {
     message:
       'cpf deve  ter os nove primeiros dígitos agrupados em três grupos de três dígitos separados por um ponto, seguidos de um hífen e dos dois últimos dígitos',
   })
   cpf: string;
 
-  @ApiProperty({example: "rpaiva654@email.com"})
+  @ApiProperty({ example: 'rpaiva654@email.com' })
   @IsEmail({}, { message: 'email deve ser um email válido' })
   email: string;
 
-  @ApiProperty({example: "+5511994532331"})
+  @ApiProperty({ example: '+5511994532331' })
   @IsOptional()
   @IsPhoneNumber('BR', {
     message: 'telefone deve ter o formato <CODIGO_DO_PAIS><DDD>XXXXXXXX',
   })
   telefone: string;
 
-  @ApiProperty({example: "BENEFICIARIO"})
+  @ApiProperty({ example: 'BENEFICIARIO' })
   @IsEnum(TiposDeBeneficiario, { message: 'tipo deve ser BENEFICIARIO' })
   tipo: TipoDeBeneficiario;
 
-  @ApiProperty({example: "Outrasenha321$"})
+  @ApiProperty({ example: 'Outrasenha321$' })
   @IsStrongPassword(
     {},
     {
@@ -92,18 +95,18 @@ export class SignUpBeneficiarioDto {
 }
 
 export class SignInUsuarioDto {
-  @ApiProperty({example: "cleber.guimaraes@email.com"})
+  @ApiProperty({ example: 'cleber.guimaraes@email.com' })
   @IsEmail({}, { message: 'email deve ser um email válido' })
   email: string;
 
-  @ApiProperty({example: "Senha123!"})
+  @ApiProperty({ example: 'Senha123!' })
   @IsNotEmpty({ message: 'senha não pode ser omitida' })
   @IsString({ message: 'senha deve ser uma string' })
   senha: string;
 }
 
 export class SignInBeneficiarioDto {
-  @ApiProperty({example: "987.654.321-25"})
+  @ApiProperty({ example: '987.654.321-25' })
   @IsString()
   @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/, {
     message:
@@ -112,12 +115,12 @@ export class SignInBeneficiarioDto {
   @IsOptional()
   cpf?: string;
 
-  @ApiProperty({example: "rpaiva654@email.com"})
+  @ApiProperty({ example: 'rpaiva654@email.com' })
   @IsEmail({}, { message: 'email deve ser um email válido' })
   @IsOptional()
   email?: string;
 
-  @ApiProperty({example: "Outrasenha321$"})
+  @ApiProperty({ example: 'Outrasenha321$' })
   @IsNotEmpty({ message: 'senha não pode ser omitida' })
   @IsString({ message: 'senha deve ser uma string' })
   senha: string;
