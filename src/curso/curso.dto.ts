f5663095a5061baffa52c42a85172443f8dffc17
Iev Mesquita <ievsouza622@gmail.com>
2023-11-06 21:25:23 -0300

    Criada a documentação inicial da parte de cursos da API #22

--- src/curso/curso.dto.ts
+++ src/curso/curso.dto.ts
@@ -1,12 +1,15 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCursoDto {
+  @ApiProperty({ example: 'Psicologia' })
   @IsString()
   @IsNotEmpty()
   nome: string;
 }
 
 export class UpdateCursoDto {
+  @ApiProperty({ example: 'Direito' })
   @IsString()
   @IsNotEmpty()
   nome: string;
