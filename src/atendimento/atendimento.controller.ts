5e1bfe467f5ab5a45d87dbfbddd1f8ee8fcef08f
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-11-08 23:19:16 -0300

    implementação dos endpoints para CRUD de atendimento #33

--- src/atendimento/atendimento.controller.ts
+++ src/atendimento/atendimento.controller.ts
@@ -1,7 +1,24 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
 import { AtendimentoService } from './atendimento.service';
-import { AtendimentoResponse, CreateAtendimentoDto } from './atendimento.dto';
+import {
+  AtendimentoFilterDto,
+  AtendimentoResponse,
+  CreateAtendimentoDto,
+  GetAtendimentoByCpfDto,
+  UpdateDataAtendimentoDto,
+} from './atendimento.dto';
 
 @Controller('atendimentos')
 @UseGuards(JwtGuard)
@@ -19,5 +36,40 @@ export class AtendimentoController {
     } catch (err) {
       return { message: JSON.stringify(err) };
     }
+  }
+
+  @Get()
+  async getAllAtendimentos(): Promise<AtendimentoResponse[]> {
+    return this.atendimentoService.getAllAtendimentos();
+  }
+
+  @Get()
+  async getAtendimentosByCpf(
+    @Body() dto: GetAtendimentoByCpfDto,
+  ): Promise<AtendimentoResponse[]> {
+    return this.atendimentoService.getAtendimentosByBeneficiarioCpf(dto);
+  }
+
+  @Get('by-date')
+  async getAtendimentosByDate(
+    @Body() dto: AtendimentoFilterDto,
+  ): Promise<AtendimentoResponse[]> {
+    return this.atendimentoService.getAtendimentosByDateRange(dto);
+  }
+
+  @Delete(':id')
+  async deleteAtendimentoById(
+    @Request() req,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.atendimentoService.deleteAtendimentoById(id);
+  }
+
+  @Patch('update-date/:id')
+  async updateAtendimentoDate(
+    @Param('id', ParseIntPipe) atendimentoId: number,
+    @Body() dto: UpdateDataAtendimentoDto,
+  ): Promise<AtendimentoResponse> {
+    return this.atendimentoService.updateAtendimentoDate(atendimentoId, dto);
   }
 }
--- src/atendimento/atendimento.dto.ts
+++ src/atendimento/atendimento.dto.ts
@@ -1,20 +1,10 @@
-import { Atendimento, Beneficiario, Programa, Usuario } from '@prisma/client';
-import { IsDateString, IsNotEmpty, IsString, Matches } from 'class-validator';
-
-export class CreateAtendimentoDto {
-  @IsDateString()
-  data: Date;
-
-  @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/, {
-    message:
-      'cpf deve  ter os nove primeiros dígitos agrupados em três grupos de três dígitos separados por um ponto, seguidos de um hífen e dos dois últimos dígitos',
-  })
-  cpfBeneficiario: string;
-
-  @IsString()
-  @IsNotEmpty()
-  nomePrograma: string;
-}
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  Matches,
+} from 'class-validator';
 
 export type AtendimentoResponse = {
   atendimento: {
@@ -38,3 +28,39 @@ export type AtendimentoResponse = {
     };
   };
 };
+
+export class CreateAtendimentoDto {
+  @IsDateString()
+  data: Date;
+
+  @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/, {
+    message:
+      'cpf deve  ter os nove primeiros dígitos agrupados em três grupos de três dígitos separados por um ponto, seguidos de um hífen e dos dois últimos dígitos',
+  })
+  cpfBeneficiario: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nomePrograma: string;
+}
+
+export class GetAtendimentoByCpfDto {
+  @Matches(/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/, {
+    message:
+      'cpf deve  ter os nove primeiros dígitos agrupados em três grupos de três dígitos separados por um ponto, seguidos de um hífen e dos dois últimos dígitos',
+  })
+  cpfBeneficiario: string;
+}
+
+export class AtendimentoFilterDto {
+  @IsDateString()
+  dataInicio: Date;
+
+  @IsDateString()
+  dataFim: Date;
+}
+
+export class UpdateDataAtendimentoDto {
+  @IsDateString()
+  novaData: string;
+}
