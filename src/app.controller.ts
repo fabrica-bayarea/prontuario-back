cb1bf98b75be84c37581400d62851c224157f50b
ArthurRabel <arthur.rabel@outlook.com>
2024-09-21 09:41:13 -0300

    test e2e corrigido, ref #43

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @ApiExcludeEndpoint()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -15,7 +17,8 @@ import { AtendimentoModule } from './atendimento/atendimento.module';
     ProgramaModule,
     AtendimentoModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
+
